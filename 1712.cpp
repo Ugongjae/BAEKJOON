4ac8bd1faf04b20da5c233c6c52fa992c80686c3
Ugongjae <turtle0191@naver.com>
2021-03-02 22:14:19 +0900

    구현

---
+++ 1712.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int A,B,C;
+    cin>>A>>B>>C;
+    
+    if(B>=C){
+        cout<<-1<<endl;
+    }else{
+        cout<<A/(C-B)+1<<endl;
+    }
+
+    return 0;
+}
